55bf6a58011da0483951ed14b757c25ab3e74d1b
chencong <1085576236@qq.com>
2018-07-03 15:26:33 +0800

    first commit

---
+++ src/main/java/com/andlinks/cloud/andlinkscloudeureka/AndlinksCloudEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.andlinks.cloud.andlinkscloudeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndlinksCloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndlinksCloudEurekaApplication.class, args);
+	}
+}
