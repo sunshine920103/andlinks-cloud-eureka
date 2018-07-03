4042efb1efa904154866295e774d1a8897b55d90
chencong <1085576236@qq.com>
2018-07-03 19:02:13 +0800

    修改包名

--- src/main/java/com/andlinks/cloud/eureka/AndlinksCloudEurekaApplication.java
+++ src/main/java/com/andlinks/cloud/eureka/AndlinksCloudEurekaApplication.java
@@ -1,4 +1,4 @@
-package com.andlinks.cloud.andlinkscloudeureka;
+package com.andlinks.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/andlinks/cloud/eureka/AndlinksCloudEurekaApplicationTests.java
+++ src/test/java/com/andlinks/cloud/eureka/AndlinksCloudEurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.andlinks.cloud.andlinkscloudeureka;
+package com.andlinks.cloud.eureka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
