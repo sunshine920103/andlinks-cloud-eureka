4e198eaeef38fafc33f3bc5b93f9c28048401851
chencong <1085576236@qq.com>
2018-07-03 18:51:46 +0800

    完成eureka注册中心基本功能

--- src/main/java/com/andlinks/cloud/andlinkscloudeureka/AndlinksCloudEurekaApplication.java
+++ src/main/java/com/andlinks/cloud/andlinkscloudeureka/AndlinksCloudEurekaApplication.java
@@ -2,8 +2,10 @@ package com.andlinks.cloud.andlinkscloudeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AndlinksCloudEurekaApplication {
 
 	public static void main(String[] args) {
